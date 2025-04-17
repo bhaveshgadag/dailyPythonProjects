d51e04d86e015531d580adf31d3ad35c93d76512
bhaveshgadag <bhaveshgadag46@gmail.com>
2025-04-17 19:03:45 +0530

    countdown timer

---
+++ Exercise_20240829.py
@@ -0,0 +1,15 @@
+# (1) The program starts by prompting the user to enter the time in seconds (e.g., 10 seconds). 
+# (2) Then, the program starts printing out the seconds (e.g., 00:10, 00:09, etc.) one line every one second. 
+# (3) At the end, the program prints out “Time is up!”
+
+import time
+
+ctime = int(input("Enter the time in seconds: "))
+
+while ctime != 0:
+    print(f"{ctime}")
+    ctime = ctime - 1
+    time.sleep(1)
+
+else:
+    print("Time's up!")
